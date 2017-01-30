72d28a394fdf30273ad9b45ed60faaf1d0e6bd9d
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2017-01-30 17:31:46 -0600

    campo email

--- event.php
+++ event.php
@@ -6,20 +6,21 @@
     /*
         EDIT BELOW
      */
-    $to_Email       = "hello@wejam.xyz"; //Replace with your email address
+    $to_Email       = "miguel@pcuervo.com"; //Replace with your email address
     $subject        = 'Email sent from Jam site - Event'; //Subject line for emails
     /*
         EDIT ABOVE
      */
 
-	if(!isset($_POST["competitor-name"]) || !isset($_POST["competitor-lastname"]) ) {
+	if(!isset($_POST["competitor-name"]) || !isset($_POST["competitor-lastname"]) || !isset($_POST["competitor-email"]) ) {
 	    $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
 	    die($output);
 	}
 
 	//additional validation
     $user_Name        = filter_var($_POST["competitor-name"], FILTER_SANITIZE_STRING);
-    $user_Lastname       = filter_var($_POST["competitor-lastname"], FILTER_SANITIZE_STRING);
+    $user_Lastname    = filter_var($_POST["competitor-lastname"], FILTER_SANITIZE_STRING);
+    $user_Email       = filter_var($_POST["competitor-email"], FILTER_SANITIZE_EMAIL);
 
     if(strlen($user_Name)<4) {
         $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
@@ -31,13 +32,13 @@
     }
 
     //proceed with PHP email.
-    $headers = "From: hello@wejam.xyz\n" .
-    "Reply-To: hello@wejam.xyz\n" .
+    $headers = "From: $user_Email\n" .
+    "Reply-To: $user_Email\n" .
     "MIME-Version: 1.0\n" .
     "Content-Type: text/html\n" .
     "X-Mailer: PHP/" . phpversion();
 
-    $sentMail = @mail($to_Email, $subject, ' <p><strong>Name:</strong></p>'.$user_Name .' <p><strong>Last Name:</strong></p>'.$user_Lastname, $headers);
+    $sentMail = @mail($to_Email, $subject, ' <p><strong>Name:</strong></p>'.$user_Name .' <p><strong>Last Name:</strong></p>'.$user_Lastname  .' <p><strong>Email:</strong></p>'.$user_Email, $headers);
 
     if(!$sentMail) {
         $output = json_encode(array('type'=>'error', 'text' => 'Server error, could not send email. Sorry for the inconvenience, try again please.'));
